2c9e10d8fa8468ef97da300961550847955747c5
bestiadivina147 <diegopc147@gmail.com>
2024-05-02 12:28:25 +0200

    build(NormalItem.java): add class NormalItem.java

---
+++ app/src/main/java/edu/badpals/GildedRose/NormalItem.java
@@ -0,0 +1,10 @@
+package edu.badpals.GildedRose;
+
+public class NormalItem extends Item {
+
+    public NormalItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
