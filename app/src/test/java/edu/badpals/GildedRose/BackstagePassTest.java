948124f46bcebbd4854dfc7eb71b306b79891acd
bestiadivina147 <diegopc147@gmail.com>
2024-05-06 10:08:09 +0200

    test(BackstagePassTest.java): add test to BackstagePassTest.java

---
+++ app/src/test/java/edu/badpals/GildedRose/BackstagePassTest.java
@@ -0,0 +1,53 @@
+package edu.badpals.GildedRose;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BackstagePassTest {
+    @Test
+    public void testSellInUpdate(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 10, 20);
+        pass.update_item();
+        assertEquals(9, pass.getSellIn());
+    }
+    @Test
+    public void testSellInMasTen(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 11, 20);
+        pass.update_quality();
+        assertEquals(21, pass.getQuality());
+    }
+    @Test
+    public void testSellInTen(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 10, 20);
+        pass.update_quality();
+        assertEquals(22, pass.getQuality());
+    }
+    @Test
+    public void testSellInMenorTenMasFive(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 9, 20);
+        pass.update_quality();
+        assertEquals(22, pass.getQuality());
+    }
+    @Test
+    public void testSellInFive(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 5, 20);
+        pass.update_quality();
+        assertEquals(23, pass.getQuality());
+    }
+    public void testSellInMenorFiveMasZero(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 3, 20);
+        pass.update_quality();
+        assertEquals(23, pass.getQuality());
+    }
+    public void testSellInZero(){
+        BackstagePass pass = new BackstagePass("BackstagePass", 0, 20);
+        pass.update_quality();
+        assertEquals(23, pass.getQuality());
+    }
+    public void testSellInMenorZero(){
+        BackstagePass pass = new BackstagePass("BackstagePass", -1, 20);
+        pass.update_quality();
+        assertEquals(0, pass.getQuality());
+    }
+}
