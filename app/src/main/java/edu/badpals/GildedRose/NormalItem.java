59b2109a1dac2052c5d272a92d48fefd660ac55c
bestiadivina147 <diegopc147@gmail.com>
2024-05-06 09:06:17 +0200

    feat(NormalItem.java): implements interface methods to NormalItem.java

--- app/src/main/java/edu/badpals/GildedRose/NormalItem.java
+++ app/src/main/java/edu/badpals/GildedRose/NormalItem.java
@@ -1,10 +1,33 @@
 package edu.badpals.GildedRose;
 
-public class NormalItem extends Item {
+public class NormalItem extends Item implements Updateable{
 
     public NormalItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
         //TODO Auto-generated constructor stub
     }
+    @Override
+    public int getQuality() {
+        return this.quality;
+    }
+
+    @Override
+    public int getSellIn() {
+        return this.sellIn;
+    }
+    @Override
+    public void update_item() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update_item'");
+    }
+
+    @Override
+    public void update_quality() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update_quality'");
+    }
+
+
+ 
     
 }
